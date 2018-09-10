0995889ba3cfa45f1ae09ea90cf5106908732a96
loyalwilliams@163.com <2541692705@qq.com>
2018-09-10 11:13:22 +0800

    贴吧爬图

--- crawler/base/urllib2_re_xml/tieba_xpath.py
+++ crawler/base/urllib2_re_xml/tieba_xpath.py
@@ -1,17 +1,39 @@
 #coding=utf-8
 import urllib2
-import urllib
 from lxml import etree
 
 def main():
-    url= 'http://tieba.baidu.com/f?ie=utf-8&kw=%E7%BE%8E%E5%A5%B3&fr=search'
+    url= 'https://tieba.baidu.com/f?ie=utf-8&kw=%E7%81%AB%E5%BD%B1&fr=search'
     header = {"User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1 Trident/5.0;"}
     request=urllib2.Request(url,headers=header)
     html=urllib2.urlopen(request).read()
 
     #解析html为HTML文档
     selector=etree.HTML(html)
-    print selector
+    # print selector
+    links=selector.xpath('//div[@class="threadlist_title pull_left j_th_tit "]/a/@href')
+    counter = 0
+    for link in links:
+        img_url= 'https://tieba.baidu.com'+link
+        request=urllib2.Request(img_url,headers=header)
+        img_HTML=urllib2.urlopen(request).read()
+
+        images_links= etree.HTML(img_HTML).xpath('//img[@class="BDE_Image"]/@src')
+
+
+        for images_link in images_links:
+            # print images_link
+            request=urllib2.Request(images_link,headers=header)
+            img=urllib2.urlopen(request).read()
+            with open("./images/"+str(counter)+'.png',"wb") as f:
+               f.write(img)
+            counter+=1
+            print counter
+        #最多爬30多张图片
+        if (counter>=30):
+            break
+
+
 
 
 if __name__ == '__main__':
